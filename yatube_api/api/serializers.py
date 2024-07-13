64b0b0cc403e0d122b75cce9b4f8bc1fc4df0311
Grigoriy N <b.grishkevitch@yandex.ru>
2024-07-13 18:46:38 +0500

    Firts try

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,59 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+from posts.models import Comment, Group, Post, User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    post = serializers.StringRelatedField(
+        many=True,
+        read_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'post'
+        )
+        read_only_fields = ('id')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault(),
+    )
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = (
+            'post',
+            'author',
+        )
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+    )
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+        read_only_fields = (
+            'pub_date',
+            'author',
+        )
